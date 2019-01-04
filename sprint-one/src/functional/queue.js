b9b021204b29363e57b7c7939fcf826dc991e3a4
Kyruski <jamescboyett@gmail.com>
2019-01-04 16:26:49 -0600

    Implemented the constructor and started working on enqueue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,12 +1,24 @@
 var Queue = function() {
-  var someInstance = {};
+  var someInstance = function(valueToStore) {
+    const obj = {valueToStore: valueToStore};
+    // let instanceBehind;//set in storage
+    return obj;
+  };
+
+  var front = 1;
+  var back = 0;
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  var storage = {
+  };
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    if (back > front) {
+      console.log('You messed up. Empty queue.');
+    }
+    someInstance.storage[back + 1];
   };
 
   someInstance.dequeue = function() {
