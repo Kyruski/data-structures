f4d17bc00609e11b85e1b92463458a8a0cd08d1b
Kyruski <jamescboyett@gmail.com>
2019-01-05 16:48:25 -0600

    Implemented working Stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,20 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.top = 0;
+  this.storage = {};
 };
 
-
+Stack.prototype.push = function(value) {
+  this.top++;
+  this.storage[this.top] = value;
+};
+Stack.prototype.pop = function() {
+  if (this.size() === 0) {
+    return 0;
+  }
+  const poppedItem = this.storage[this.top];
+  this.top--;
+  return poppedItem;
+};
+Stack.prototype.size = function() {
+  return this.top;
+};
