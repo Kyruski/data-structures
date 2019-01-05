09383580b73f9259d7733a517f03109543610ee4
Kyruski <jamescboyett@gmail.com>
2019-01-05 16:59:31 -0600

    Implemented ES6-style queue

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,26 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.front = 0;
+    this.back = 0;
+    this.storage = {};
   }
 
+  enqueue(value) {
+    this.storage[this.back] = value;
+    this.back++;
+  }
+
+  dequeue() {
+    if (this.size() === 0) {
+      return 0;
+    }
+    const dequeuedItem = this.storage[this.front];
+    this.front++;
+    return dequeuedItem;
+  }
+
+  size() {
+    return this.back - this.front;
+  }
 }
