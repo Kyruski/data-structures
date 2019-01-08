d8f990e8209685df6f0c6410fa03fedbf7311d88
Kyruski <jamescboyett@gmail.com>
2019-01-08 13:40:37 -0600

    Adds the time complexity for the methods

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -58,6 +58,10 @@ HashTable.prototype.remove = function(k) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * getBucket: Constant
+ * insert: Linear/Constant
+ * retrieve: Linear/Consant
+ * remove: Linear/Constant
  */
 
 
