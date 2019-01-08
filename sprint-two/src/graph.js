8ebe328d4340fc8c50788dc7de9551c53971273b
Kyruski <jamescboyett@gmail.com>
2019-01-07 19:19:31 -0600

    Refactored Graph, addNodes, contains, and removeNode for better functionality

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,62 +2,54 @@
 
 // Instantiate a new graph
 var Graph = function() {
-  this.value = null;
-  this.edges = [];
+  this.nodes = [];
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
-  if (this.value === null) {
-    this.value = node;
-  } else {
-    const newNode = new Graph;
-    newNode.value = node;
-  }
+  this.newGraph = Object.create(Graph.prototype);
+  this.newGraph.value = node;
+  this.newGraph.edges = [];
+  this.nodes.push(this.newGraph);
+  // if (this.value === null) {
+  //   this.value = node;
+  // } else {
+  //   const newNode = new Graph;
+  //   newNode.value = node;
+  // }
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
-  const runType = arguments[1] || 0;
-  let isTrue = false;
-  const visitedArray = [];
-  const searchGraph = function(currentNode) {
-    if (isTrue) {
-      return;
-    }
-    visitedArray.push(currentNode);
-    if (currentNode.value === node) {
-      if (runType === 1) {
-        isTrue = currentNode;
-      } else {
-        isTrue = true;
-      }
-    } else {
-      for (let i = 0; i < currentNode.edges.length; i++) {
-        if (visitedArray.contains(currentNode.edges[i])) {
-          continue;
-        } else {
-          searchGraph(currentNode.edges[i]);
-        }
+  if (this.nodes.length === 0) {
+    return false;
+  }
+  const secondArg = arguments[1] || 0;
+  for (let i = 0; i < this.nodes.length; i++) {
+    if (this.nodes[i].value === node) {
+      if (secondArg === 0) {
+        return true;
+      } else if (secondArg === 1) {
+        return [this.nodes[i], i];
       }
     }
-    return isTrue;
-  };
-  return searchGraph(this);
+  }
+  return false;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
-  if (this.edges.length === 0) {
-    this.value = null;
-  } else {
-    let desiredNode = this.contains(node, 1);
-    for (let i = 0; i < desiredNode.edges.length; i++) {
-      const index = desiredNode[i].edges.indexOf(node);
-      desiredNode[i].edges.splice(index, 1);
+  const containsResult = this.contains(node, 1);
+  const desiredNode = containsResult[0];
+  const index = containsResult[1];
+  for (let i = 0; i < desiredNode.edges.length; i++) {
+    for (let j = 0; j < desiredNode.edges[i].edges.length; j++) {
+      if (desiredNode.edges[i].edges[j].value === node) {
+        desiredNode.edges[i].edges.splice(j, 1);
+      }
     }
-    desiredNode.edges = [];
   }
+  this.nodes.splice(index, 1);
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
